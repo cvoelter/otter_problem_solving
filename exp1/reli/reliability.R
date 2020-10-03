e4d393e26be7f2e96b2d24ba9bfe24c0655519f2
manuelbohn <manuel0bohn@gmail.com>
2020-10-03 21:37:10 +0200

    add reli exp1

---
+++ exp1/reli/reliability.R
@@ -0,0 +1,22 @@
+library(psych)
+library(readxl)
+
+rel_data <- read_xlsx("exp1/reli/160628 Otter multiaccess box_data 2016attempts_reli.xlsx", sheet = 2)
+
+# success
+
+mean(rel_data$match_success)
+
+cohen.kappa(cbind(rel_data$Success,rel_data$Success_reli))
+
+# Method
+
+mean(rel_data$match_method)
+
+cohen.kappa(cbind(rel_data$Success_Method,rel_data$Success_Method_reli))
+
+# transitions
+
+mean(rel_data$match_trans)
+
+cohen.kappa(cbind(rel_data$Number_transitions ,rel_data$Number_transitions_reli))
